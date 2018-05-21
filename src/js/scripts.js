5c7f4403e6f1684c9c28f6e1d85c29638c4e2a71
Filip Petrovic <roll4@outlook.com>
2018-05-22 01:16:14 +0200

    Webpack - removed Gulp, doing templating, styles and Vue compiling through webpack

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,12 @@
 /* ... */
+
+/* Templating */
+// require('./../../src/pug/index.pug'); 
+
+
+/* Styles */
+// require('../scss/main.scss'); 
+
+
+/* Vue */
+// require('./App.vue');
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,87 @@
 const path = require('path');
-var HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+// var HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const extractSass = new ExtractTextPlugin({
+    filename: "styles.css",
+    disable: process.env.NODE_ENV === "development"
+});
 
 module.exports = {
+	entry: './src/js/scripts.js',
     output: {
-        publicPath: "./dist/",
-        path: path.join(__dirname, "/js/"),
+        publicPath: "",
+        path: path.join(__dirname, "/dist/js/"),
         filename: "bundle.js"
-    },
+	},
+	watch: true,
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: "babel-loader",
-                query: {
+                loader: "babel-loader", 
+                options: {
                     presets: ["env"]
                 }
-            },
+			},
+			{
+				test: /\.scss$/,
+				use: ExtractTextPlugin.extract({
+					use: ['css-loader?url=false', 'sass-loader']
+				})
+			},
             {
                 test: /\.vue$/,
                 loader: 'vue-loader'
-            }
+			},
+			{
+				test: /\.pug$/,
+				loaders: [
+					"file-loader?name=../[name].html",
+					"extract-loader",
+					"html-loader",
+					"pug-html-loader"
+				]
+			}
         ]
 	},
+	resolve: {
+		alias: {
+			vue: 'vue/dist/vue.js'
+		}
+	},
 	plugins: [
-		new HardSourceWebpackPlugin()
+		new ExtractTextPlugin('../css/main.css')
 	]
 };
+
+// const path = require('path');
+// var HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+
+// module.exports = {
+//     output: {
+//         publicPath: "./dist/",
+//         path: path.join(__dirname, "/js/"),
+//         filename: "bundle.js"
+//     },
+//     module: {
+//         loaders: [
+//             {
+//                 test: /\.js$/,
+//                 exclude: /node_modules/,
+//                 loader: "babel-loader",
+//                 query: {
+//                     presets: ["env"]
+//                 }
+//             },
+//             {
+//                 test: /\.vue$/,
+//                 loader: 'vue-loader'
+//             }
+//         ]
+// 	},
+// 	plugins: [
+// 		new HardSourceWebpackPlugin()
+// 	]
+// };
